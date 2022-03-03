697967b37bed1bbb9ab8c1af946a0048e0b32781
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-03-02 20:54:10 -0600

    [updater] clases de php y java pero hay un error de arraylist casa 2/3/2022

---
+++ java/UberBlack.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.ArrayList;
+
+class UberBlack extends Car{
+    Map<String, ArrayList<String, Integer>>  typeCarAcepted;
+    ArrayList<String> seatsMaterial;
+
+    public UberBlack(String license, Account driver,Map<String, ArrayList<String, Integer>>  typeCarAcepted, ArrayList<String> seatsMaterial ){
+        super(license, driver);
+        this.typeCarAcepted = typeCarAcepted;
+        this.seatsMaterial= seatsMaterial;
+    }
+}
