1c456ed37259876fbe53057f8f456c94efda1b91
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-03-09 15:59:49 -0600

    firt update poo in JS alcaldia 9/3/2022

---
+++ poo_basico_JS/main.js
@@ -0,0 +1,13 @@
+const natalia = {
+    name: "Natalya",
+    age: 20,
+    courseAprobe: [
+        "Curso definitivo de HTML/CSS",
+        "Curso Practico de HTML/CSS"
+    ],
+};
+
+//haceque natalia apruebe otro curso
+
+natalia.courseAprobe.push("Curso de polimorfismo");
+
