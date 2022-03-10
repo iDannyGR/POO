ab86fa840c4551a22ae5b027bf88206e9a5b3c03
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-03-09 20:53:00 -0600

    [update] ya creamos prototypes en js para set y get casa 9/3/2022

--- poo_basico_JS/main.js
+++ poo_basico_JS/main.js
@@ -5,9 +5,34 @@ const natalia = {
         "Curso definitivo de HTML/CSS",
         "Curso Practico de HTML/CSS"
     ],
+
+    aprobarCurso(nuevoCurso){
+            this.courseAprobe.push(nuevoCurso);
+    }
 };
 
 //haceque natalia apruebe otro curso
 
 natalia.courseAprobe.push("Curso de polimorfismo");
+natalia.aprobarCurso('curso de closure and scope');
 
+function Student(name, age, courseAprove) {
+        this.name = name;
+        this.age = age;
+        this.courseAprobe = courseAprove;
+        // this.aprovarCurso = ()=>{
+
+        // }
+}
+Student.prototype.aprobarCurso =function (nuevoCurso){
+    this.courseAprobe.push(nuevoCurso);
+ }
+
+const juanita = new Student(
+    'Jorge Daniel Garcia',
+    32,
+    ["curso de pingaloca",
+        "curso de pingaloca 23423",
+    "curso de ingles basico para pendejos"]
+)
+juanita.aprobarCurso('cursso de pasta dental y chano')
