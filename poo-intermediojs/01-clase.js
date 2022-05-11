dff3010ffd93593c4758ad16589490091c6822e1
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-05-11 09:59:32 -0600

    [start] curso intermeio de poo en JS

---
+++ poo-intermediojs/01-clase.js
@@ -0,0 +1,10 @@
+const objetito = {
+    name: "Carlitos",
+    email: "carlitosmazzaroli@gmail.com",
+    age: 16,
+}
+
+
+console.log(Object.keys(objetito))
+console.log(Object.getOwnPropertyNames(objetito));
+console.log(Object.entries(objetito));
