3e6d76474167e331833198187f7d5f53268d4e50
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-06-06 21:26:30 -0600

    [reto con async y await falta] aun no se termina casa 6/63/2022

--- apirest_consumoJS/main.js
+++ apirest_consumoJS/main.js
@@ -1,9 +1,16 @@
-document.addEventListener('load',() =>{
+const URL = 'https://rickandmortyapi.com/api/character';
 
-
-    imgbox = document.getElementsByTagName('img');
+async function persons(number){
+    const response = await fetch(URL)
+    const data = await response.json();
+    let tHead = document.querySelector('p');
+    let imgbox = document.querySelector('img'); 
+    tHead.innerHTML = data.results[number].name;
+    imgbox.src = data.results[number].image;
+}    
     
+let rNumber =Math.floor(Math.random() * (826 - 0)) + 0;
+const button= document.getElementById('ramdom')
+console.log(rNumber)
+button.onclick = persons(rNumber)
 
-
-
-})
