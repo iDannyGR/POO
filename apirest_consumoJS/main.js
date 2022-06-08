6a1af978f2b636618b2e152e63942a50cac2ed37
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-06-08 15:59:46 -0600

    [rediseño] alcaldia 8/6/22

--- apirest_consumoJS/main.js
+++ apirest_consumoJS/main.js
@@ -1,11 +1,14 @@
  const URL = 'https://rickandmortyapi.com/api/character/';
-
+//para colocar una api key se escribe en la URL 
+// api_key=e704b6ae-17c0-4d01-9e4a-5e1f6c9d0bf4
+//es la key que solicite en cat API
 // function ramdomNumber(n1, n2){
 //     return parseInt((Math.random() * (n2 - n1 + 1)) + n1);
 // }
 const body = document.getElementById('container');
+let queryParam = '?page=5';
 async function rmperson(){
-        const conexion =await fetch(URL);
+        const conexion =await fetch(URL+queryParam);
         const data = await conexion.json(); 
         
         data.results.forEach(item => {
