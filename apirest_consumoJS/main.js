2d69ef95025e93c85ca009e9bcb18b6b8feb6f19
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-06-09 15:59:28 -0600

    [favorits]creando funcion de favoritos para api y se cambio a la de gatos alcaldia 9/6/2022

--- apirest_consumoJS/main.js
+++ apirest_consumoJS/main.js
@@ -1,30 +1,37 @@
- const URL = 'https://rickandmortyapi.com/api/character/';
+ const URL = 'https://api.thecatapi.com/v1/';
 //para colocar una api key se escribe en la URL 
-// api_key=e704b6ae-17c0-4d01-9e4a-5e1f6c9d0bf4
+const API_KEY='api_key=e704b6ae-17c0-4d01-9e4a-5e1f6c9d0bf4';
+let queryParam = '?limit=10';
 //es la key que solicite en cat API
 // function ramdomNumber(n1, n2){
 //     return parseInt((Math.random() * (n2 - n1 + 1)) + n1);
 // }
 const body = document.getElementById('container');
-let queryParam = '?page=11';
-async function rmperson(){
-        const conexion =await fetch(URL+queryParam);
+
+async function getData(){
+        const conexion =await fetch(`${URL}images/search${queryParam}&${API_KEY}`);
         const data = await conexion.json(); 
-        
-        data.results.forEach(item => {
-            const cart= document.createElement('div')
-            const  imgSrc = document.createElement('img')
-            imgSrc.src= item.image;
-            const title = document.createElement('p')
-            const addFavorite = document.createElement()
-            title.textContent= item.name
-            cart.append(title, imgSrc);
-            body.append(cart)
-        });
+        data.forEach((item) => {
+            const container= document.createElement('div');
+            const  imgSrc = document.createElement('img');
+            imgSrc.src= item.url;
+            const addFavorite = document.createElement('button');
+            addFavorite.textContent='Favourite';
+            container.append(addFavorite, imgSrc);
+            body.append(container)
+         });
 
 }
 
-rmperson()
+async function favourites(){
+    const conexion =await fetch(`${URL}favourites${queryParam}&${API_KEY}`);
+    const data = await conexion.json(); 
+    console.log(data);
+
+}
+
+favourites()
+getData()
 
 // fetch(URL)
 //     .then(res => res.json())
