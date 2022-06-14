a917849e862d35630bcfa6ac4430396a10b2799d
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-06-14 16:10:25 -0600

    revision de header y http resquest alcaldia 14/6/2022

--- apirest_consumoJS/main.js
+++ apirest_consumoJS/main.js
@@ -14,66 +14,80 @@ async function getData(){
     try {
         const conexion =await fetch(`${URL}images/search${queryParam}&${API_KEY}`);
         const data = await conexion.json(); 
-        const allData = new DocumentFragment(); //crea un framento de document
+        const allData = [];
+       // const allData = new DocumentFragment(); //crea un framento de document
         //no tiene padre y no afecta el load de todo el DOM
         data.forEach((item) => {
             const container= document.createElement('div');
-            container.className='containerImg'
             const  imgSrc = document.createElement('img');
+            const addFavorite = document.createElement('button');
+
             imgSrc.src= item.url;
             imgSrc.className='imageLoad';
-            const addFavorite = document.createElement('button');
+            container.className='containerImg'
             addFavorite.textContent='Favourite';
             addFavorite.className = 'button';
-            //addFavorite.setAttribute('onclick', 'favourite()');
+            addFavorite.addEventListener('click', favourite.bind('id', item.id));
+            // .bind() instancia una funcion que le asigna al this de esa funcion el valor
+            // entregado como parametro en este caso item.id
             container.append(addFavorite, imgSrc);
-            allData.append(container);
+            allData.push(container);
+            console.table(item)
          });
-         body.append(allData);
+         body.append(...allData);
     } catch (error) {
         errorMessage.textContent('Error de conexion')
         throw Error(error);    
     }
 }
 async function loadCatFavorite(){
+    favoritesContainer.innerHTML = ''
     const response = await fetch(`${URL}favourites?${API_KEY}`);
     const data = await response.json();
+    const dataFavorites = [];
         data.forEach(item => {
             const art= document.createElement('div');
             const  img = document.createElement('img');
             const btn = document.createElement('button');
+
             btn.className='button';
             art.className = 'containerImg';
             btn.textContent='Eliminar';
+            btn.addEventListener('click', deleteMichi.bind('id', item.id))
             img.className='imageLoad';
             img.src = item.image.url;
             art.append(btn,img);
-            favoritesContainer.append(art);
+            dataFavorites.push(art)
+            console.log(item)
         })
-    
+    favoritesContainer.append(...dataFavorites)
 }
 async function favourite(id){
     try {
+        
         const conexion =await fetch(`${URL}favourites?${API_KEY}`,{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
             },
-            body: JSON.stringify({image_id:'b09'})
+            body: JSON.stringify({image_id:id})
+            
         });
-            const data = await conexion.json(); 
-           
-
-
+        loadCatFavorite()
     } catch (error) {
         console.table(error)
         throw Error(error)
     }
     
 }
-
-async function deleteMichi(){
-
+async function deleteMichi(id){
+    try {
+        const conexion =await fetch(`${URL}favourites/${id}?${API_KEY}`,{method:'DELETE'});
+        loadCatFavorite()
+    } catch (error) {
+        console.table(error)
+        throw Error(error)
+    }
 }
 
 getData()
